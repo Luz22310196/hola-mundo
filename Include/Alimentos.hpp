5550a88a8b7675960e2822b0f8f1189404af0ce3
Luz22310196 <160083181+Luz22310196@users.noreply.github.com>
2024-02-23 02:09:35 +0000

    version 2

---
+++ Include/Alimentos.hpp
@@ -0,0 +1,15 @@
+class Alimentos
+{
+private:
+    int energia;
+public:
+    Alimentos(int energia) {
+        this->energia=energia;
+    }
+    ~Alimentos() {}
+    int ExtraerEnergia(){
+        return energia;
+    }
+};
+
+
---
+++ Include/Mascota.hpp
@@ -0,0 +1,14 @@
+class Mascota
+{
+private:
+   private:
+    int vida;
+    public:
+    Mascota(){
+        this->vida =0;
+    }
+    -Mascota(){}
+    void comer (Alimento alimento){
+        vida += alimento.ExtraerEnergia()
+    }
+);
